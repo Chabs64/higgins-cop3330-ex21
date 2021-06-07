252a803af20c4739aa35da3f72b9d4005b3b42c7
chad <chadhiggins6@gmail.com>
2021-06-06 21:37:08 -0400

    exercise 21

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -24,10 +24,74 @@ Use a switch or case statement for this program.
 Use a single output statement for this program.
  */
 
-public class App 
+import java.util.Scanner;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Please enter the number of the month:");
+        int numMonth = in.nextInt();
+        String thatMonth;
+
+        switch(numMonth)
+        {
+            case 1:
+                thatMonth = "January";
+                break;
+
+            case 2:
+                thatMonth = "February";
+                break;
+
+            case 3:
+                thatMonth = "March";
+                break;
+
+            case 4:
+                thatMonth = "April";
+                break;
+
+            case 5:
+                thatMonth = "May";
+                break;
+
+            case 6:
+                thatMonth = "June";
+                break;
+
+            case 7:
+                thatMonth = "July";
+                break;
+
+            case 8:
+                thatMonth = "August";
+                break;
+
+            case 9:
+                thatMonth = "September";
+                break;
+
+            case 10:
+                thatMonth = "October";
+                break;
+
+            case 11:
+                thatMonth = "November";
+                break;
+
+            case 12:
+                thatMonth = "December";
+                break;
+
+
+            default:
+                throw new IllegalStateException("Unexpected value: " + numMonth);
+        }
+
+        System.out.println("The name of the month is "+thatMonth);
+
     }
 }
